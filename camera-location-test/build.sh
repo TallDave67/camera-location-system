5ff6253fe83ffde2295eb99dab6d8931016e3952
TallDave67 <david.bellis1967@gmail.com>
2022-01-25 02:16:33 +0100

    cleaner form of build script for test project

--- camera-location-test/build.sh
+++ camera-location-test/build.sh
@@ -7,16 +7,15 @@ else
   mkdir $DIRECTORY
 fi
 cd $DIRECTORY
-DIRECTORY="lib"
-if [ -d "$DIRECTORY" ]
-then
-    echo "$DIRECTORY exists on your filesystem."
-else
-  mkdir $DIRECTORY
-fi
 
-cp "../../camera-location-engine/build/src/camera/libcamera_location_engine-src-camera.a" lib/.
+# generate the build system
 cmake ../
+
+# manually copy over the library file we need to a folder in this build ...
+cp "../../camera-location-engine/build/src/camera/libcamera_location_engine-src-camera.a" lib/.
+# ... and add it's name to end of the one-line list of library files in the link.txt file
 truncate --size -1 ./CMakeFiles/camera_location_test.dir/link.txt
 echo -n "lib/libcamera_location_engine-src-camera.a " >> ./CMakeFiles/camera_location_test.dir/link.txt
+
+# run the build
 make VERBOSE=1
